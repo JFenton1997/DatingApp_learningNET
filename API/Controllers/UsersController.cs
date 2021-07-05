90e3b5ea89fae5e828cce3c39a040a0e93cba268
JFenton1997 <jfenton1997@gmail.com>
2021-07-05 21:33:53 +0100

    API Authentication Added End of Section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
@@ -8,9 +9,8 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -20,16 +20,20 @@ namespace API.Controllers
         }
 
         //task is create thread to run seperately, wait for result, use async methods;
+
         [HttpGet]
-        public  async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();   
+            return await _context.Users.ToListAsync();
         }
 
+
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            return await _context.Users.FindAsync(id);   
+            return await _context.Users.FindAsync(id);
         }
 
     }
